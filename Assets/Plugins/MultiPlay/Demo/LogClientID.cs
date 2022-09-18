edbe99a5ae0a90b49bfb25f34a95499e3172c054
Muammar81 <Muammar.Yacoob@gmail.com>
2022-09-18 21:46:36 +0100

    Editor code restricted to Windows

--- Assets/Plugins/MultiPlay/Demo/LogClientID.cs
+++ Assets/Plugins/MultiPlay/Demo/LogClientID.cs
@@ -1,11 +1,12 @@
 #if UNITY_EDITOR_WIN
 using UnityEngine;
+using MultiPlay.Editor;
 
 public class LogClientID : MonoBehaviour
 {
     private void Start()
     {
-        int clientIndex = 0;//GetCurrentClientIndex();
+        int clientIndex = Utils.GetCurrentClientIndex();
         if (clientIndex == 0) Debug.Log("MultiPlay is running on: Main Project/Server");
         else Debug.Log($"MultiPlay is running on Client: {clientIndex}");
     }
